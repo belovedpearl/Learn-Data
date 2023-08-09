0347477b1e074e4f812ddfe91b5aa14e1b1339c8
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-08-09 04:41:36 +0000

    Delete single record on db

--- sql-crud.py
+++ sql-crud.py
@@ -95,28 +95,46 @@ oluwaseun_omisakin = Programmer(
 # programmer = session.query(Programmer).filter_by(id=10).first()
 # programmer.famous_for = "World President"
 
-# To update multiple records in a database
-people = session.query(Programmer)
-for person in people:
-    if person.gender == "F":
-        person.gender = "Female"
-    elif person.gender == "M":
-        person.gender = "Male"
-    else:
-        print("Gender not defined")
-    session.commit()
-
 # commit our session to the database
-#session.commit()
+# session.commit()
+
+# To update multiple records in a database
+# people = session.query(Programmer)
+# for person in people:
+#     if person.gender == "F":
+#         person.gender = "Female"
+#     elif person.gender == "M":
+#         person.gender = "Male"
+#     else:
+#         print("Gender not defined")
+#     session.commit()
+
+# deleting a single record
+fname = input("Enter a first name: ")
+lname = input("Enter a last name: ")
+programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+#defensive programming
+if programmer is not None:
+    print("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
+    confirmation = input("Are you sure you want to delete this record? (y/n) ")
+    if confirmation.lower() == "y":
+        session.delete(programmer)
+        session.commit()
+        print("Programmer has been deleted")
+    else:
+        print("Programmer not deleted")
+else:
+    print("No records found")
+
 
 # query the database to find all Programmers
-programmers = session.query(Programmer)
-for programmer in programmers:
-    print(
-        programmer.id,
-        programmer.first_name + " " + programmer.last_name,
-        programmer.gender,
-        programmer.nationality,
-        programmer.famous_for,
-        sep=" | "
-    )
+# programmers = session.query(Programmer)
+# for programmer in programmers:
+#     print(
+#         programmer.id,
+#         programmer.first_name + " " + programmer.last_name,
+#         programmer.gender,
+#         programmer.nationality,
+#         programmer.famous_for,
+#         sep=" | "
+#     )
